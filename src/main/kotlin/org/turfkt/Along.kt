26ef0b5e34bfa4f9e0870a1c47c438033936dc41
Brian Hudson <brian.r.hudson@gmail.com>
2019-02-25 12:51:50 -0500

    Implements measurement:along
    Resolves #1

---
+++ src/main/kotlin/org/turfkt/Along.kt
@@ -0,0 +1,25 @@
+package org.turfkt
+
+fun LineString.along(distance: Double, units: String): Feature<Point> {
+    var travelled = 0.0
+    for(i in coordinates.indices) {
+        if(distance >= travelled && i == coordinates.lastIndex) break
+
+        if(distance == travelled) return Feature(Point(coordinates[i]))
+
+        if(travelled > distance) {
+            val overshot = distance - travelled
+            val direction = coordinates[i].bearing(coordinates[i - 1]) - 180
+            val interpolated = coordinates[i].destination(overshot, direction, units)
+            return Feature(Point(interpolated))
+        }
+
+        travelled += coordinates[i].distance(coordinates[i + 1])
+    }
+
+    return Feature(Point(coordinates.last()))
+}
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Feature<LineString>.along(distance: Double, units: String): Feature<Point> =
+        geometry.along(distance, units)
